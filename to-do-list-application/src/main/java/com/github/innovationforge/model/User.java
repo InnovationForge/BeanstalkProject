42265c4fc91de1636a60e71c0b38c34c93208298
Niranjan Rath <niranjan.rath@gmail.com>
2024-03-11 09:57:53 +0100

    updated with no arg constructor

--- to-do-list-application/src/main/java/com/github/innovationforge/model/User.java
+++ to-do-list-application/src/main/java/com/github/innovationforge/model/User.java
@@ -2,6 +2,7 @@ package com.github.innovationforge.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 
 import java.util.Collection;
@@ -10,6 +11,7 @@ import java.util.List;
 import org.springframework.security.core.GrantedAuthority;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class User {
     private String username;
